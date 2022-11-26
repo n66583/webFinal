f01813b9eb380e4a75363f48c3fbbb4fb3b50c82
Hossain sadeqi <119123595+n66583@users.noreply.github.com>
2022-11-26 19:44:43 +0330

    Add files via upload

---
+++ admin/admin.php
@@ -0,0 +1,15 @@
+<?php session_start(); if ($_SESSION["user_name"] != "admin") { exit(); } ?>
+<!DOCTYPE HTML> 
+<html>
+<head>
+</head>
+<body>
+
+<p>Hello admin!</p>
+<p>https://3114551641.cloudylink.com</p>
+<p>h191583</p>
+<p>JWdSDMoxwn5Z08</p>
+
+
+</body>
+</html>
---
+++ user/user.php
@@ -0,0 +1,12 @@
+<?php session_start(); if ($_SESSION["user_name"] != "user") { exit(); } ?>
+<!DOCTYPE HTML> 
+<html>
+<head>
+</head>
+<body>
+
+<p>Hello user!</p>
+
+
+</body>
+</html>
